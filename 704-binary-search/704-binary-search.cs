7028930d2b3adbd4eac3a5113fb71edcf5c80e90
Feroz Ahmmed <froghramar@gmail.com>
2022-01-13 10:27:53 +0600

    Time: 128 ms (64.89%), Space: 43.6 MB (38.65%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int Search(int[] nums, int target)
+    {
+        int left = 0, right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            var mid = (left + right) / 2;
+
+            if (target > nums[mid])
+            {
+                left = mid + 1;
+            }
+            else if (target < nums[mid])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+}
