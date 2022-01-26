657c619bf18d272de53ecbc1d92875c24121c2bd
Feroz Ahmmed <froghramar@gmail.com>
2022-01-26 11:20:38 +0600

    Time: 104 ms (82.40%), Space: 41.5 MB (45.01%) - LeetHub

--- 136-single-number/136-single-number.cs
+++ 136-single-number/136-single-number.cs
@@ -1,22 +1,12 @@
 public class Solution {
     public int SingleNumber(int[] nums)
     {
-        const int ranges = 30000;
-        var count = new int[2 * ranges + 1];
-        
+        var result = 0;
         foreach (var num in nums)
         {
-            count[num + ranges]++;
-        }
-        
-        for (var i = 0; i < count.Length; i++)
-        {
-            if (count[i] == 1)
-            {
-                return i - ranges;
-            }
+            result = result ^ num;
         }
 
-        return -1;
+        return result;
     }
 }
