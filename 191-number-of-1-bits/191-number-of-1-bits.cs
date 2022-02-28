89f4a1a79a95129d47fff362b7fcedd57fc84857
Feroz Ahmmed <froghramar@gmail.com>
2022-02-28 09:36:18 +0600

    Time: 20 ms (95.02%), Space: 23.9 MB (50.38%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cs
+++ 191-number-of-1-bits/191-number-of-1-bits.cs
@@ -1,14 +1,8 @@
+using System.Runtime.Intrinsics.X86;
+
 public class Solution {
     public int HammingWeight(uint n)
     {
-        var count = 0;
-
-        while (n > 0)
-        {
-            count += (int) (n & 1);
-            n >>= 1;
-        }
-
-        return count;
+        return (int) Popcnt.PopCount(n);
     }
 }
