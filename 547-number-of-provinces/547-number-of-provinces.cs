0f875d01314488ed0b621d9fe57f001bcc72bb20
Feroz Ahmmed <froghramar@gmail.com>
2022-03-07 12:58:31 +0600

    Time: 144 ms (60.77%), Space: 39.9 MB (34.32%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cs
+++ 547-number-of-provinces/547-number-of-provinces.cs
@@ -1,34 +1,78 @@
 public class Solution {
     public int FindCircleNum(int[][] isConnected)
     {
-        var count = 0;
-        var visited = new bool[isConnected.Length];
+        var n = isConnected.Length;
+        var unionFind = new UnionFind(n);
 
-        for (var i = 0; i < isConnected.Length; i++)
+        for (var i = 0; i < n; i++)
         {
-            CountCircleNum(isConnected, i, ref count, true, visited);
+            for (var j = i + 1; j < n; j++)
+            {
+                if (isConnected[i][j] == 1)
+                {
+                    unionFind.Union(i, j);
+                }
+            }
         }
-        
-        return count;
+
+        return unionFind.GetDistinctRoots();
+    }
+}
+
+public class UnionFind
+{
+    private int[] _parents;
+    private int[] _ranks;
+    
+    public UnionFind(int n)
+    {
+        _parents = new int[n];
+        _ranks = new int[n];
+
+        for (var i = 0; i < n; i++)
+        {
+            _parents[i] = i;
+            _ranks[i] = 1;
+        }
     }
 
-    private void CountCircleNum(int[][] isConnected, int x, ref int count, bool startNew, bool[] visited)
+    public int Find(int x)
     {
-        if (visited[x])
+        if (x == _parents[x])
         {
-            return;
+            return x;
         }
 
-        visited[x] = true;
+        return _parents[x] = Find(_parents[x]);
+    }
 
-        if (startNew)
-        {
-            count++;
-        }
+    public void Union(int x, int y)
+    {
+        var rootX = Find(x);
+        var rootY = Find(y);
 
-        for (var i = 0; i < isConnected.Length; i++)
+        if (_ranks[rootX] > _ranks[rootY])
         {
-            if (x != i && isConnected[x][i] == 1) CountCircleNum(isConnected, i, ref count, false, visited);
+            _parents[rootY] = rootX;
         }
+        else if (_ranks[rootX] < _ranks[rootY])
+        {
+            _parents[rootX] = rootY;
+        }
+        else
+        {
+            _parents[rootY] = rootX;
+            _ranks[rootX]++;
+        }
+    }
+
+    public int GetDistinctRoots()
+    {
+        var roots = new int[_parents.Length];
+        for (var i = 0; i < _parents.Length; i++)
+        {
+            roots[i] = Find(i);
+        }
+        return roots.Distinct().Count();
     }
 }
