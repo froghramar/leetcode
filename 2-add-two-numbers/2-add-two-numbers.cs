538ef440b5cb57836200ee4161588f3b525c2d51
Feroz Ahmmed <froghramar@gmail.com>
2022-03-10 09:22:02 +0600

    Time: 154 ms (31.83%), Space: 39.5 MB (38.91%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cs
+++ 2-add-two-numbers/2-add-two-numbers.cs
@@ -9,57 +9,59 @@
  *     }
  * }
  */
-public class Solution {
-    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+public class Solution
+{
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
         ListNode resultFirst = null;
-            ListNode resultLast = null;
+        ListNode resultLast = null;
 
-            var carriage = 0;
-            while (l1 != null || l2 != null)
+        var carriage = 0;
+        while (l1 != null || l2 != null)
+        {
+            var currentSum = GetDigitSum() + carriage;
+            var newNode = new ListNode(currentSum % 10);
+            carriage = currentSum / 10;
+
+            AddNewNode(newNode);
+
+            int GetDigitSum()
             {
-                var currentSum = GetDigitSum() + carriage;
-                var newNode = new ListNode(currentSum % 10);
-                carriage = currentSum / 10;
-
-                AddNewNode(newNode);
-
-                int GetDigitSum()
+                var currentValue = 0;
+                if (l1 != null)
                 {
-                    var currentValue = 0;
-                    if (l1 != null)
-                    {
-                        currentValue += l1.val;
-                        l1 = l1.next;
-                    }
-
-                    if (l2 != null)
-                    {
-                        currentValue += l2.val;
-                        l2 = l2.next;
-                    }
-
-                    return currentValue;
+                    currentValue += l1.val;
+                    l1 = l1.next;
                 }
-            }
 
-            if (carriage > 0)
+                if (l2 != null)
+                {
+                    currentValue += l2.val;
+                    l2 = l2.next;
+                }
+
+                return currentValue;
+            }
+        }
+
+        if (carriage > 0)
+        {
+            AddNewNode(new ListNode(carriage));
+        }
+
+        void AddNewNode(ListNode newNode)
+        {
+            if (resultLast == null)
             {
-                AddNewNode(new ListNode(carriage));
+                resultFirst = resultLast = newNode;
             }
-
-            void AddNewNode(ListNode newNode)
+            else
             {
-                if (resultLast == null)
-                {
-                    resultFirst = resultLast = newNode;
-                }
-                else
-                {
-                    resultLast.next = newNode;
-                    resultLast = newNode;
-                }
+                resultLast.next = newNode;
+                resultLast = newNode;
             }
+        }
 
-            return resultFirst;
+        return resultFirst;
     }
 }
