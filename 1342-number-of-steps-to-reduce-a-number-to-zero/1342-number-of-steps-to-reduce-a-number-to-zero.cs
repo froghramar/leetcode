a02398c80dc13bf68044ecd4bfc9b005bfd504b4
Feroz Ahmmed <froghramar@gmail.com>
2022-05-27 08:29:00 +0600

    Time: 22 ms (85.03%), Space: 26.6 MB (10.07%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int NumberOfSteps(int num)
+    {
+        var count = 0;
+        while (num > 0)
+        {
+            if (num % 2 == 0)
+            {
+                num /= 2;
+            }
+            else
+            {
+                num--;
+            }
+
+            count++;
+        }
+        return count;
+    }
+}
